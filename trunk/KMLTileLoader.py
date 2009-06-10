124c3ddc7c0386fba6e8b23d24c6cece21934851
brianhone <brianhone@92ce86bc-1cc3-11de-bcda-2bb9902f8029>
2009-06-10 03:13:53 +0000

    Interim commit of KMLTileLoader

--- trunk/KMLTileLoader.py
+++ trunk/KMLTileLoader.py
@@ -35,16 +35,16 @@
 
 import time, sys, os, string, random, math
 import pprint
-from TileMap import TileMap
+from TileLoader import TileLoader
 import KMLParser
 
 false = 0
 true = 1
 debug = 1
 
-class KMLTileMap( TileMap ):
-  def __init__(self, (x,y,z), (sizeX, sizeY), kmlFile, cacheUrl, zoomlevel ):
-    TileMap.__init__( self, (x,y,z), (sizeX, sizeY), cacheUrl )
+class KMLTileMap( TileLoader ):
+  def __init__(self, (sizeX, sizeY), kmlFile, cacheUrl, zoomlevel ):
+    TileLoader.__init__( self, (sizeX, sizeY), cacheUrl )
     self.zoomLevel    = zoomlevel # minzoomlevel
     self.kmlFile      = kmlFile
     self.kmlPoints    = {}
@@ -52,12 +52,8 @@ class KMLTileMap( TileMap ):
     self.initKML()
   # end __init__
 
-  def getTile( self, x, y, z ):
-    pass
-  # end getTile
-
-  def createTile( self, x, y, z ):
-    tileStr = self.getEmptyTile()
+  def fetchTile( self, x, y, z ):
+    tileArr = self.getEmptyTile()
     #print tileStr
     for key, value in self.kmlPoints.items():
       lat = float( value[ "LAT" ] )
@@ -70,77 +66,7 @@ class KMLTileMap( TileMap ):
       if res != None:
           pixX, pixY = res[0], res[1]
           tileStr = self.addTextToTile( pixX, pixY, value[ "NAME" ], tileStr )
-    self.saveTileToCache( x, y, z, tileStr )
   # end createTile
-
-  def addTextToTile( self,pixX, pixY, text, tileStr ):
-    #pos = self.sizeX * pixY + pixX
-    pos = pixX - len( text )/2
-    if debug:
-      print "pixX = %s, len(text) = %s, pos = %s" % (pixX, len(text), pos)
-    if pos < 0:
-      pos = 0
-    for c in text:
-      tileStr[pixY][pos] = c
-      pos = pos + 1
-    return tileStr
-  # end addTextToTile
-
-  def getEmptyTile( self ):
-    arr = []
-    for y in range(self.sizeY):
-        arr.append( [" "] * self.sizeX )	
-    #return [" "] * ( ( self.sizeX ) * ( self.sizeY ) )
-    return arr
-  # end getMepthTile
-
-  def saveTileToCache( self, x, y, z, tileStr ):
-    fname = self.cacheUrl+ "/%s/%s/%s.txt" % ( z,x,y )
-    dirParts = string.split( fname, "/" )[:-1]
-    curdir='./'
-    nextdir=''
-    for dirname in dirParts:
-      if nextdir == '':
-        nextdir = dirname
-      else:
-        nextdir=nextdir+os.sep+dirname
-
-      if dirname not in os.listdir(curdir):
-        if debug:
-          print "trying to make " + nextdir
-        try:
-          os.mkdir(nextdir)
-        except:
-          print "Couldn't make directory %s" % nextdir
-          return
-      curdir = nextdir
-
-    try:
-      output_file = open( fname, "w" )
-    except:
-      print "Couldn't open file %s" % fname
-
-    for row_num in range(self.sizeY):
-        output_file.write( string.join(tileStr[row_num],"" ) ) # BTH - not right
-        output_file.write( "\n" )
-    output_file.close() 
-  # end saveTileToCache
-
-  def loadTileFromCache( self ):
-    pass
-  # end loadTileFromCache
-
-  def makeFakeTile( self, fname ):
-    a = open( fname, "w" )
-    for i in range ( self.sizeY ):
-      s = [ " " ] * self.sizeX
-      for j in range( self.sizeX ):
-        if j%5 == 0:
-          s[j] = "x"
-      a.write( string.join( s, "" ) )
-      a.write( "\n" )
-    a.close()
-  # end makeFakeFile
 
   def initKML( self ):
     """ Load cities and countries from a kml file - ./kml_files.txt lists kml files to load"""
@@ -157,5 +83,5 @@ class KMLTileMap( TileMap ):
 
 if __name__=="__main__":
   #def __init__(self, (x,y,z), (sizeX, sizeY), kmlFile, cacheUrl ):
-  T = KMLTileMap((0,0,1), (56,56),  "us_states.kml", "test_cache", 0 )
-  T.createTile( 0,0,1 )
+  T = KMLTileMap((56,56),  "us_states.kml", "test_cache", 0 )
+  print T.getTile( 0,0,1 )
